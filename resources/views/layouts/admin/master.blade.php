b7242668c22395b9f6c79f4d9a08747ce8221326
ITGCS <helmi.satria@great.sch.id>
2025-06-13 13:46:44 +0700

    master

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -9,11 +9,358 @@
 </head>
 
 <style>
-    body {
-        font-family: "DynaPuff", system-ui !important;
-        font-weight: 400;
-        font-style: normal;
+    * {
+    font-size: 12px;
+     font-family: "DynaPuff", system-ui;
+    font-weight: 400;
+    font-style: normal;
+}
+
+.divider:after,
+.divider:before {
+    content: "";
+    flex: 1;
+    height: 1px;
+    background: #eee;
+}
+.h-custom {
+    height: calc(100% - 73px);
+}
+@media (max-width: 450px) {
+    .h-custom {
+        height: 100%;
     }
+}
+
+.dynamic-select {
+    display: block;
+    overflow-y: auto;
+    min-height: 38px; /* Sesuai dengan tinggi form-control */
+    max-height: 150px; /* Batas tinggi maksimal */
+    resize: vertical; /* Memungkinkan pengguna menyesuaikan ukuran */
+}
+
+
+.file-upload {
+    background-color: #ffffff;
+    width: 100%;
+    margin: 0 auto;
+    padding: 50px;
+}
+
+.download-template-btn {
+    width: 100%;
+    margin: 0;
+    color: #fff;
+    background: #1fb264;
+    border: none;
+    padding: 10px;
+    border-radius: 4px;
+    border-bottom: 4px solid #15824b;
+    transition: all 0.2s ease;
+    outline: none;
+    text-transform: uppercase;
+    font-weight: 700;
+}
+
+.download-template-btn:hover {
+    background: #1aa059;
+    color: #ffffff;
+    transition: all 0.2s ease;
+    cursor: pointer;
+}
+
+.download-template-btn:active {
+    border: 0;
+    transition: all 0.2s ease;
+}
+
+.file-upload-content {
+    display: none;
+    text-align: center;
+}
+
+.file-upload-input {
+    position: absolute;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    outline: none;
+    opacity: 0;
+    cursor: pointer;
+}
+
+.image-upload-wrap {
+    margin-top: 20px;
+    border: 4px dashed #1fb264;
+    position: relative;
+}
+
+.image-dropping,
+.image-upload-wrap:hover {
+    background-color: #1fb264;
+    border: 4px dashed #ffffff;
+}
+
+.image-title-wrap {
+    padding: 0 15px 15px 15px;
+    color: #222;
+}
+
+.drag-text {
+    text-align: center;
+}
+
+.drag-text h3 {
+    font-weight: 100;
+    text-transform: uppercase;
+    color: #15824b;
+    padding: 60px 0;
+}
+
+.file-upload-image {
+    max-height: max-content;
+    max-width: max-content;
+    margin: auto;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    padding: 20px;
+}
+
+.remove-image {
+    width: 200px;
+    margin: 0;
+    color: #fff;
+    background: #cd4535;
+    border: none;
+    padding: 15px;
+    border-radius: 4px;
+    border-bottom: 4px solid #b02818;
+    transition: all 0.2s ease;
+    outline: none;
+    text-transform: uppercase;
+    font-weight: 700;
+}
+
+.upload-image {
+    width: 200px;
+    margin: 0;
+    color: #fff;
+    background: #0183a3;
+    border: none;
+    padding: 15px;
+    border-radius: 4px;
+    border-bottom: 4px solid #00566b;
+    transition: all 0.2s ease;
+    outline: none;
+    text-transform: uppercase;
+    font-weight: 700;
+}
+
+.remove-image:hover {
+    background: #c13b2a;
+    color: #ffffff;
+    transition: all 0.2s ease;
+    cursor: pointer;
+}
+
+.remove-image:active {
+    border: 0;
+    transition: all 0.2s ease;
+}
+
+.custom-control-label::before,
+.custom-control-label::after {
+    top: 0.8rem;
+    width: 1.25rem;
+    height: 1.25rem;
+}
+
+.bg-image {
+    /* background-image: url("images/lgn.jpg"); */
+    background-image: url("images/depan_sekolah_new.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh; /* Membuat background memenuhi seluruh layar */
+    position: relative;
+}
+
+/* Overlay dengan opacity */
+.bg-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.7); /* Mengatur opacity agar background tidak terlalu gelap */
+    z-index: 1;
+}
+
+.container-fluid {
+    position: relative;
+    z-index: 2; /* Menempatkan konten di atas overlay */
+}
+
+/* ANIMATION */
+.small-box {
+    transition: transform 0.1s ease-in-out;
+}
+
+.small-box:hover {
+    cursor: pointer;
+    transform: scale(1.04);
+}
+
+.box {
+    transition: transform 0.3s ease-in-out;
+}
+
+.box:hover {
+    transform: scale(0.95);
+}
+
+/* END ANIMATION */
+
+.grid-container-profil {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 10px;
+    padding: 5px;
+}
+
+/* Untuk seluruh elemen yang memiliki scrollbar */
+*::-webkit-scrollbar {
+    width: 2px; /* Mengatur lebar scrollbar */
+    height: 6px; /* Mengatur tinggi scrollbar untuk horizontal */
+}
+
+/* Styling thumb (bagian yang bisa digeser) */
+*::-webkit-scrollbar-thumb {
+    background-color: orange; /* Warna scrollbar */
+    border-radius: 0px; /* Membuat sudut lebih halus */
+}
+
+/* Styling track (jalur scrollbar) */
+*::-webkit-scrollbar-track {
+    background-color: #f0f0f0; /* Warna latar belakang track scrollbar */
+    border-radius: 0px;
+}
+
+
+.custom-card-student {
+    /* padding: 40px 80px; */
+    background-color: #A8DADC;
+    color: #000;
+    border-radius: 30% 40% 30% 40% / 40% 30% 40% 30% ;
+    border: 3px solid #1D3357;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: transform 0.3s;
+    box-shadow: 3px 3px 10px rgba(63, 5, 222, 0.1);
+    margin: 10px
+}
+
+.custom-card-student:hover {
+    cursor: pointer;
+    opacity: 1;
+    transform: rotate(-2deg) scale(1.05);
+}
+
+.dynapuff-regular {
+    font-family: "DynaPuff", system-ui;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    font-variation-settings:
+    "wdth" 100;
+}
+
+.btn-menu-top {
+    margin-top: 0px;
+    padding: 0px;
+    width: 72px;
+    height: 32px;
+    background-color: #ffde9e;
+    color: #1D3557;
+    clip-path: polygon(
+    10% 0%, 20% 10%, 35% 5%, 50% 15%, 65% 5%, 80% 10%, 90% 0%, 
+    100% 20%, 95% 35%, 100% 50%, 95% 65%, 100% 80%, 90% 100%, 
+    75% 90%, 60% 95%, 50% 85%, 40% 95%, 25% 90%, 10% 100%, 
+    0% 80%, 5% 65%, 0% 50%, 5% 35%, 0% 20%
+    );
+    border: 3px solid #ffde9e;
+    overflow: hidden;
+    transition: transform 0.3s;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
+}
+
+.btn-menu-top:hover {
+    transform: scale(1.2) rotate(-3deg);
+    color: #e47600;
+    animation: bounce 0.3s ease-in-out;
+}
+
+.hero-title {
+    font-size: 1.2rem;
+    color: #ff9000;
+    letter-spacing: 1px;
+    /* text-shadow: 2px 2px #ffcc00; */
+}
+
+.chewy-regular {
+    font-family: "Chewy", system-ui;
+    font-weight: 400;
+    font-style: normal;
+}
+
+.shadow-soft {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1),
+    0 2px 4px rgba(0, 0, 0, 0.08);
+  border-radius: 12px;
+}
+
+.icon-assessment {
+    background-image: url("images/assessment.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh; /* Membuat background memenuhi seluruh layar */
+    position: relative;
+}
+
+/* Contoh bentuk radius non-standar (menggunakan clip-path atau border-radius custom) */
+.swal2-popup.rounded-swal {
+    padding: 15px;
+    border-radius: 86% 64% 74% 76% / 76% 80% 80% 94%;
+    background-color: #ffde9e;
+    transition: transform 0.3s;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
+    border: 3px solid #ffcc00;
+}
+
+
+.custom-swal-style {
+    padding: 20px;
+    background-color: #ffde9e !important;
+    color: #000 !important;
+    border-radius: 86% 64% 74% 76% / 76% 80% 80% 94% !important;
+    border: 3px solid #ffcc00 !important;
+    overflow: hidden !important;
+    display: grid !important;
+    align-items: center !important;
+    justify-content: center !important;
+    transition: transform 0.3s !important;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1) !important;
+}
+
+
+
+
+
 </style>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
