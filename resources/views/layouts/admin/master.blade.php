974830bf4e989589206c08fd8f0c26cdf451ad67
ITGCS <helmi.satria@great.sch.id>
2025-06-13 13:45:09 +0700

    style

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Caveat+Brush&family=Chewy&family=DynaPuff&family=Lora:ital,wght@0,400..700;1,400..700&family=Patrick+Hand&family=Vollkorn:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('style.css')}}">
     @include('layouts.header')
     @livewireStyles
 </head>
