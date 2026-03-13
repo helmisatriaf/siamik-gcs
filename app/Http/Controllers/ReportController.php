e1a9512060fc554e78ebef00e50c1ab848897f40
Helmi Satria <73867058+helmisatriaf@users.noreply.github.com>
2026-03-13 16:49:33 +0700

    Fix query for monthly titles in ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4849,7 +4849,7 @@ class ReportController extends Controller
             }
 
             $monthly = MonthlyActivity::where('grades', '=', 'lower')->where('semester', '=', $semester)->where('academic_year', '=', $academic_year)->whereIn('month', $monthActivity)->take(3)->get();
-            $monthlyTitle = MonthlyActivity::where('grades', '=', 'lower')->whereIn('month', $monthActivity)->take(3)->pluck('name')->toArray();
+            $monthlyTitle = MonthlyActivity::where('grades', '=', 'lower')->where('semester', '=', $semester)->where('academic_year', '=', $academic_year)->whereIn('month', $monthActivity)->take(3)->pluck('name')->toArray();
 
             //dd($monthlyTitle);
             foreach ($monthlyTitle as &$title) {
