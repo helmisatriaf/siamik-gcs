f98635f63f31d10f6ecadcc80a50f5db498a8497
ITGCS <helmi.satria@great.sch.id>
2026-06-18 08:53:35 +0700

    day and days semester 2 pdf report card

--- resources/views/components/report/pdf/semester2-pdf.blade.php
+++ resources/views/components/report/pdf/semester2-pdf.blade.php
@@ -166,9 +166,9 @@ $grade_name = $student->grade_name;
                 </tr>
                 <tr>
                     <td style="text-align:right;border: 1px solid black;padding-right:8px;border-left: solid 1px black;">Days Absent:</td>
-                    <td style="border: 1px solid black;padding-left:8px;" colspan="3">{{ $attendance[0]['days_absent'] }} day</td>
+                    <td style="border: 1px solid black;padding-left:8px;" colspan="3">{{ $attendance[0]['days_absent'] }} {{ $attendance[0]['days_absent'] <= 1 ? 'day' : 'days' }}</td>
                     <td style="text-align:right;border: 1px solid black;padding-right:8px;" colspan="2">Total Days Absent:</td>
-                    <td style="border: 1px solid black;padding-left:8px;border-right: solid 1px black;" colspan="2">{{ $attendance[0]['days_absent'] }}  days</td>
+                    <td style="border: 1px solid black;padding-left:8px;border-right: solid 1px black;" colspan="2">{{ $attendance[0]['days_absent'] }} {{ $attendance[0]['days_absent'] <= 1 ? 'day' : 'days' }}/td>
                 </tr>
                 <tr>
                     <td style="text-align:right;border: 1px solid black;padding-right:8px;border-left: solid 1px black;">Times Late:</td>
